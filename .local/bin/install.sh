4998519610567949d44d843bc635ba02dcd7fe6f
Sjoerd Thijsse <sjoerdthijsse@gmail.com>
2021-02-01 23:06:05 +0100

    feat: even more install things

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -90,9 +90,9 @@ install_yay() {
 install_npm() {
     if [ -n "$npmflag" ]; then
         printf '%s\n' "installing nvm, npm & node..."
-        # mkdir -p $HOME/.config/nvm
         XDG_CONFIG_HOME="$HOME/.config"
         NVM_DIR="$XDG_CONFIG_HOME/nvm"
+        mkdir -p "$NVM_DIR"
 
         nvm="https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh"
         curl -o- "$nvm" -s | bash > /dev/null 2>&1
@@ -123,7 +123,7 @@ install_gems() {
 install_nvim() {
     if [ -n "$nvimflag" ]; then
         printf '%s\n' "installing nvim plugins..."
-        nvim -c :PluginInstall +qall
+        nvim -c :PlugInstall +qall > /dev/null
     fi
 }
 
